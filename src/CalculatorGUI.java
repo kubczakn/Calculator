49e9354543db86bb7e85535ae69c90ed869b057d
kubczakn <kubczakn@umich.edu>
2020-05-17 17:46:03 -0400

    zero button works with text label

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.text.*;
 
 public class CalculatorGUI extends JFrame
 {
     private JPanel mainPanel;
-    private JTextPane textPane;
     private JButton decimalButton;
     private JButton zeroButton;
     private JButton negativeButton;
@@ -24,17 +27,30 @@ public class CalculatorGUI extends JFrame
     private JButton clearButton;
     private JPanel textPanel;
     private JPanel buttonPanel;
+    private JLabel textLabel;
 
-    private CalculatorGUI(String title) {
+    public CalculatorGUI(String title) {
         super(title);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+        final String[] labelText = { " " };
+        textLabel.setText(labelText[0]);
+        textLabel.setBackground(Color.white);
+        zeroButton.addActionListener(new ActionListener()
+        {
+            @Override public void actionPerformed(ActionEvent e)
+            { // insert a 0 into the text pane
+                labelText[0] = labelText[0].concat("0");
+                textLabel.setText(labelText[0]);
+            }
+        });
     }
 
     public static void main(String[] args) {
         JFrame frame = new CalculatorGUI("Java Calculator");
+
         frame.setVisible(true);
     }
 
