571275905e5f1536a17161dc4e6807b0938752f4
kubczakn <kubczakn@umich.edu>
2020-05-17 14:35:22 -0400

    more gui appearance changes

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -3,25 +3,27 @@ import javax.swing.*;
 public class CalculatorGUI extends JFrame
 {
     private JPanel mainPanel;
-    private JTextPane textPane1;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
-    private JButton button12;
-    private JButton button13;
-    private JButton button14;
-    private JButton button15;
-    private JButton button16;
-    private JButton button10;
-    private JButton button11;
-    private JButton button17;
-    private JButton button18;
+    private JTextPane textPane;
+    private JButton decimalButton;
+    private JButton zeroButton;
+    private JButton negativeButton;
+    private JButton equalButton;
+    private JButton addButton;
+    private JButton multiplyButton;
+    private JButton divideButton;
+    private JButton backButton;
+    private JButton threeButton;
+    private JButton twoButton;
+    private JButton oneButton;
+    private JButton fourButton;
+    private JButton fiveButton;
+    private JButton sixButton;
+    private JButton nineButton;
+    private JButton eightButton;
+    private JButton sevenButton;
+    private JButton clearButton;
+    private JPanel textPanel;
+    private JPanel buttonPanel;
 
     public CalculatorGUI(String title) {
         super(title);
